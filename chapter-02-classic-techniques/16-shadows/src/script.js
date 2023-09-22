ec059d8445a87cdc403c28fda1b7a5873b70c1b9
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2023-09-22 19:15:44 +0200

    Complete chapter 2, lesson 17 and start lesson 18

--- chapter-02-classic-techniques/16-shadows/src/script.js
+++ chapter-02-classic-techniques/16-shadows/src/script.js
@@ -1,14 +1,14 @@
-import "./style.css";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "dat.gui";
+import * as dat from 'dat.gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import './style.css';
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const bakedShadow = textureLoader.load("/textures/bakedShadow.jpg");
-const simpleShadow = textureLoader.load("/textures/simpleShadow.jpg");
+const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg');
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg');
 
 /**
  * Base
@@ -17,7 +17,7 @@ const simpleShadow = textureLoader.load("/textures/simpleShadow.jpg");
 const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
 const scene = new THREE.Scene();
@@ -27,16 +27,16 @@ const scene = new THREE.Scene();
  */
 // Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
 directionalLight.position.set(2, 2, -1);
-gui.add(directionalLight, "intensity").min(0).max(1).step(0.001);
-gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
-gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001);
-gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001);
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001);
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.width = 1024;
 directionalLight.shadow.mapSize.height = 1024;
@@ -51,7 +51,7 @@ directionalLight.shadow.camera.far = 6;
 scene.add(directionalLight);
 
 const directionalLightCameraHelper = new THREE.CameraHelper(
-  directionalLight.shadow.camera,
+  directionalLight.shadow.camera
 );
 directionalLightCameraHelper.visible = false;
 scene.add(directionalLightCameraHelper);
@@ -91,8 +91,8 @@ scene.add(pointLightCameraHelper);
  */
 const material = new THREE.MeshStandardMaterial();
 material.roughness = 0.7;
-gui.add(material, "metalness").min(0).max(1).step(0.001);
-gui.add(material, "roughness").min(0).max(1).step(0.001);
+gui.add(material, 'metalness').min(0).max(1).step(0.001);
+gui.add(material, 'roughness').min(0).max(1).step(0.001);
 
 /**
  * Objects
@@ -113,7 +113,7 @@ const sphereShadow = new THREE.Mesh(
     color: 0x000000,
     transparent: true,
     alphaMap: simpleShadow,
-  }),
+  })
 );
 sphereShadow.rotation.x = -Math.PI * 0.5;
 sphereShadow.position.y = plane.position.y + 0.01;
@@ -127,7 +127,7 @@ const sizes = {
   height: window.innerHeight,
 };
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
@@ -149,7 +149,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
-  100,
+  100
 );
 camera.position.x = 1;
 camera.position.y = 1;
